8b6634eff88cf58bf608a5006453fb7faac6d779
uyorum <uyorum.pub@gmail.com>
2017-08-12 14:11:01 +0900

    Finding target_device in before filter

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ require 'irkit'
 environment = ENV['RACK_ENV'] || 'development'
 config_file "config/#{environment}.yml"
 
+before %r{/devices/([\w]+).*} do
+  @target_device = settings.devices.select {|device| device['name'] == params['captures'].first}.first
+  unless @target_device
+    halt 404, { message: 'device not found' }.to_json
+  end
+end
+
 get '/devices' do
   devices = []
   settings.devices.each do |device|
@@ -13,41 +20,25 @@ get '/devices' do
   devices.to_json
 end
 
-get '/devices/:name' do
-  target_device = settings.devices.select {|device| device['name'] == params[:name]}.first
-  if target_device
-    return { name: target_device['name'] }.to_json
-  else
-    status 404
-    return { message: 'device not found' }.to_json
-  end
+get '/devices/:device_name' do
+  return { name: @target_device['name'] }.to_json
 end
 
-get '/devices/:name/commands' do
-  target_device = settings.devices.select {|device| device['name'] == params[:name]}.first
-  unless target_device
-    status 404
-    return { message: 'device not found' }.to_json
-  end
-  return (target_device['commands'] || [] ).keys.to_json
+get '/devices/:device_name/commands' do
+  return (@target_device['commands'] || [] ).keys.to_json
 end
 
-post '/devices/:name/exec' do
+post '/devices/:device_name/exec' do
   payload = JSON.parse(request.body.read)
-  target_device = settings.devices.select {|device| device['name'] == params[:name]}.first
-  unless target_device
-    status 404
-    return { message: 'device not found' }.to_json
-  end
 
-  ir_json = target_device['commands'][payload['command']]
+  ir_json = @target_device['commands'][payload['command']]
   unless ir_json
     status 400
     return { message: "unknown command" }.to_json
   end
 
   ir_data = IRKit::Response.new(JSON.parse(ir_json))
-  irkit = IRKit::Device.new(address: target_device['address'])
+  irkit = IRKit::Device.new(address: @target_device['address'])
   response = irkit.post_messages(ir_data)
   unless response.response.code == "200"
     status 500
